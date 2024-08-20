f4828b179d32570d3a3d8a645ccc867b7959486a
Deven-14 <deven.paramaj@gmail.com>
2024-08-21 00:22:25 +0530

    sol: 141. Linked List Cycle

---
+++ 141. Linked List Cycle/solution.py
@@ -0,0 +1,63 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+
+        node = head
+        while node != None and node not in visited:
+            visited.add(node)
+            node = node.next
+        
+        if node in visited:
+            return True
+        
+        return False
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+
+        node = head
+        while node and node not in visited: # * using implicit bool conditions are faster
+            visited.add(node)
+            node = node.next
+        
+        if node in visited:
+            return True
+        
+        return False
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        
+        return False
