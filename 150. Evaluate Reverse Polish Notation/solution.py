41bfa35df65e5fcebc4472e6b7716a10a31ab2fb
Deven-14 <deven.paramaj@gmail.com>
2024-08-27 23:48:29 +0530

    sol: 150. Evaluate Reverse Polish Notation

---
+++ 150. Evaluate Reverse Polish Notation/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            match token:
+                case '+':
+                    second, first = stack.pop(), stack.pop()
+                    stack.append(first + second)
+                case '-':
+                    second, first = stack.pop(), stack.pop()
+                    stack.append(first - second)
+                case '*':
+                    second, first = stack.pop(), stack.pop()
+                    stack.append(first * second)
+                case '/':
+                    second, first = stack.pop(), stack.pop()
+                    stack.append(int(first / second))
+                case _:
+                    stack.append(int(token))
+        
+        return stack[0]
