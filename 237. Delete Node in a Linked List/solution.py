9bfb69ee6bccccf07a931070f4e648d0463dbbcf
Deven-14 <deven.paramaj@gmail.com>
2024-12-17 01:29:17 +0530

    sol: 237. Delete Node in a Linked List

---
+++ 237. Delete Node in a Linked List/solution.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        next_node = node.next
+        next_next_node = next_node.next
+
+        node.val = next_node.val
+        node.next = next_next_node
+        
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
