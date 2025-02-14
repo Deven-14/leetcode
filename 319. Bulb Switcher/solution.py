4c320ae7d87b7d083b6494ca56350a4db36af38d
Deven-14 <deven.paramaj@gmail.com>
2025-02-15 02:00:31 +0530

    sol: 319. Bulb Switcher

---
+++ 319. Bulb Switcher/solution.py
@@ -0,0 +1,6 @@
+import math
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(math.sqrt(n))
+
+# https://leetcode.com/problems/bulb-switcher/description/comments/1564812
