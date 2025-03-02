5b1d5accdbee7933a2e9cf8837d8d2d68ae54860
Deven-14 <deven.paramaj@gmail.com>
2025-03-02 21:31:51 +0530

    sol: 119. Pascal's Triangle II

--- 119. Pascal's Triangle II/solution.py
+++ 119. Pascal's Triangle II/solution.py
@@ -10,3 +10,17 @@ class Solution:
             prev_row = row
 
         return prev_row
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        
+        prev_row = [1, 1]
+        for _ in range(2, rowIndex+1):
+            row = [1]
+            row.extend(num1 + num2 for num1, num2 in pairwise(prev_row))
+            row.append(1)
+            prev_row = row
+        
+        return prev_row
