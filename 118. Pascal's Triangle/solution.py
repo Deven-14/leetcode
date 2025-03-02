f14325d9437b0a493fc3688b343b4d9bf31ae630
Deven-14 <deven.paramaj@gmail.com>
2025-03-02 21:27:39 +0530

    sol: 118. Pascal's Triangle

--- 118. Pascal's Triangle/solution.py
+++ 118. Pascal's Triangle/solution.py
@@ -12,3 +12,20 @@ class Solution:
         
         return rows
             
+from itertools import pairwise
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 1:
+            return [[1]]
+        
+        rows = [[1], [1, 1]]
+        prev_row = rows[-1]
+        row = [1]
+        for _ in range(3, numRows+1):
+            row = [1]
+            row.extend(num1 + num2 for num1, num2 in pairwise(prev_row))
+            row.append(1)
+            rows.append(row)
+            prev_row = row
+        
+        return rows
