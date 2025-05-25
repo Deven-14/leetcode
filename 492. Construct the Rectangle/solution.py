9bf773668c61f1e30f85f5157ef63478f7bdb0f9
Deven-14 <deven.paramaj@gmail.com>
2025-05-25 12:44:19 +0530

    sol: 492. Construct the Rectangle

---
+++ 492. Construct the Rectangle/solution.py
@@ -0,0 +1,9 @@
+import math
+class Solution:
+
+    def constructRectangle(self, area: int) -> List[int]:
+        for w in range(int(math.sqrt(area)), 0, -1):
+            if area % w == 0:
+                return [area // w, w]
+        
+        
