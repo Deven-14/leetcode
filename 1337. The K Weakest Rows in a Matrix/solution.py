d47a04e53028710bbdc3f21b1894c02c4e7ae532
Deven-14 <deven.paramaj@gmail.com>
2025-10-30 23:30:27 +0530

    sol: 1337. The K Weakest Rows in a Matrix

---
+++ 1337. The K Weakest Rows in a Matrix/solution.py
@@ -0,0 +1,4 @@
+from heapq import nsmallest
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        return nsmallest(k, range(len(mat)), key=lambda i: sum(mat[i]))
