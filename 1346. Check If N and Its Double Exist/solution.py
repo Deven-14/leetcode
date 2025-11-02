84b22d9c3a7dbf99738bfefd6a172d96a73eb16f
Deven-14 <deven.paramaj@gmail.com>
2025-11-02 18:39:39 +0530

    sol: 1346. Check If N and Its Double Exist

---
+++ 1346. Check If N and Its Double Exist/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        nums = set()
+        for num in arr:
+            if num * 2 in nums:
+                return True
+            if num & 1 == 0 and num // 2 in nums:
+                return True
+            
+            nums.add(num)
+        
+        return False
