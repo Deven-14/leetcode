788bc1866bbe77d9033cba762cb342c81af93ecd
Deven-14 <deven.paramaj@gmail.com>
2026-02-11 09:34:10 +0530

    docs(binary-search): l is one greater

--- LEARNINGS/binary search.py
+++ LEARNINGS/binary search.py
@@ -1,16 +1,18 @@
 
 # * Upper Bound
+# * (arr[:l] <= target, arr[l:] > target), arr[:l] - so l is one greater
+# * bisect.bisect_right(nums, target)
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) # * NO -1, important coz l can be len(nums) as well
+        l, r = 0, len(nums) # * NO -1, important coz l can be len(nums) as well, because l can be one greater for arr[:l]
 
         while l < r:
             m = l + ((r - l) // 2)  
             if nums[m] > target:
                 r = m
             elif nums[m] <= target:
-                l = m + 1
+                l = m + 1                      # * (arr[:l] <= target, arr[l:] > target), arr[:l] - so l is one greater
         return l - 1 if (l and nums[l - 1] == target) else -1
 
 # * for upper bound, "=" should go to the "l" as upper bound should give the element greater than target
@@ -18,23 +20,24 @@ class Solution:
 # * VISE-VERSA for "lower bound"
 
 
-# bisect.bisect_right(nums, target)
+
 
 # * Lower Bound
+# * (arr[:l] < target, arr[l:] >= target), arr[:l] - so l is one greater
+# * bisect.bisect_left(nums, target)
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) # * NO -1, important coz l can be len(nums) as well
+        l, r = 0, len(nums) # * NO -1, important coz l can be len(nums) as well, because l can be one greater for arr[:l]
 
         while l < r:
             m = l + ((r - l) // 2)  
             if nums[m] >= target:
                 r = m
             elif nums[m] < target:
-                l = m + 1
+                l = m + 1                     # * (arr[:l] < target, arr[l:] >= target), arr[:l] - so l is one greater
         return l if (l < len(nums) and nums[l] == target) else -1
 
-# bisect.bisect_left(nums, target)
 
 # eg. [1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8]
 # target = 4
