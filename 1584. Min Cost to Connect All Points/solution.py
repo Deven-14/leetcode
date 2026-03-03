027a14793d419822c352f81eda6889339ebb5198
Deven-14 <deven.paramaj@gmail.com>
2026-03-03 10:13:07 +0530

    sol: 1584. Min Cost to Connect All Points

--- 1584. Min Cost to Connect All Points/solution.py
+++ 1584. Min Cost to Connect All Points/solution.py
@@ -93,3 +93,142 @@ class Solution:
                 break
         
         return distance
+
+
+
+from collections import defaultdict
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        adj = defaultdict(dict)
+
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(i + 1, n):
+                x2, y2 = points[j]
+                d = abs(x2 - x1) + abs(y2 - y1)
+                adj[i][j] = adj[j][i] = d
+        
+        min_heap = [(0, 0)]
+        distance = 0
+        visited = set()
+        
+        while len(visited) < n:
+            curr_distance, i = heapq.heappop(min_heap)
+            if i in visited:
+                continue
+            
+            distance += curr_distance
+            visited.add(i)
+
+            for j, adj_distance in adj[i].items():
+                if j in visited:
+                    continue
+                heapq.heappush(min_heap, (adj_distance, j))
+                
+        return distance
+                
+
+
+from collections import defaultdict
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        adj = [[0] * n for _ in range(n)]
+
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(i + 1, n):
+                x2, y2 = points[j]
+                d = abs(x2 - x1) + abs(y2 - y1)
+                adj[i][j] = adj[j][i] = d
+        
+        min_heap = [(0, 0)]
+        distance = 0
+        visited = set()
+        
+        while len(visited) < n:
+            curr_distance, i = heapq.heappop(min_heap)
+            if i in visited:
+                continue
+            
+            distance += curr_distance
+            visited.add(i)
+
+            for j in range(n):
+                if j in visited:
+                    continue
+                heapq.heappush(min_heap, (adj[i][j], j))
+                
+        return distance
+                
+
+
+from collections import defaultdict
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        adj = [[0] * n for _ in range(n)]
+
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(i + 1, n):
+                x2, y2 = points[j]
+                d = abs(x2 - x1) + abs(y2 - y1)
+                adj[i][j] = adj[j][i] = d
+        
+        min_heap = [(0, 0)]
+        distances = [float('inf')] * n
+        distance = 0
+        visited = set()
+        
+        while len(visited) < n:
+            curr_distance, i = heapq.heappop(min_heap)
+            if i in visited:
+                continue
+            
+            distance += curr_distance
+            visited.add(i)
+
+            for j in range(n):
+                if j in visited or distances[j] < adj[i][j]:
+                    continue
+                distances[j] = adj[i][j]
+                heapq.heappush(min_heap, (adj[i][j], j))
+                
+        return distance
+
+
+from collections import defaultdict
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        min_heap = [(0, 0)]
+        distances = [float('inf')] * n
+        distance = 0
+        visited = [False] * n
+        count = 0
+        
+        while count < n:
+            curr_distance, i = heapq.heappop(min_heap)
+            if visited[i]:
+                continue
+            
+            distance += curr_distance
+            visited[i] = True
+            count += 1
+            x1, y1 = points[i]
+
+            for j in range(n):
+                if visited[j]:
+                    continue
+                adj_distance = abs(points[j][0] - x1) + abs(points[j][1] - y1)
+                if distances[j] < adj_distance:
+                    continue
+                distances[j] = adj_distance
+                heapq.heappush(min_heap, (adj_distance, j))
+            
+                
+        return distance
+
+
