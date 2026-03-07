5cedc98702035f448ab9f1dadf92b26622724561
Deven-14 <deven.paramaj@gmail.com>
2026-03-07 17:21:09 +0530

    sol: 78. Subsets

--- 78. Subsets/solution.py
+++ 78. Subsets/solution.py
@@ -66,3 +66,24 @@ class Solution:
                 sub_sets.append(subset)
             
         return sub_sets
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        all_subsets = []
+        subset = []
+        n = len(nums)
+        nums.sort()
+        
+        def backtrack(i):
+            all_subsets.append(subset[:])
+
+            for j in range(i, n):
+                if j > i and nums[j] == nums[j-1]:
+                    continue
+                subset.append(nums[j])
+                backtrack(j + 1)
+                subset.pop()
+        
+        backtrack(0)
+        return all_subsets
