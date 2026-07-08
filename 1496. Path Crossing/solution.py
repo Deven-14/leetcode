ed9406c47ddddc33b61ba48163b7a8f9fbf1df16
Deven-14 <deven.paramaj@gmail.com>
2026-07-08 07:11:37 +0100

    sol: 1496. Path Crossing

---
+++ 1496. Path Crossing/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        visited = { (0, 0) }
+        x, y = 0, 0
+
+        for direction in path:
+            match direction:
+                case 'N':
+                    y += 1
+                case 'S':
+                    y -= 1
+                case 'E':
+                    x += 1
+                case 'W':
+                    x -= 1
+            if (x, y) in visited:
+                return True
+            visited.add((x, y))
+        
+        return False
+        
