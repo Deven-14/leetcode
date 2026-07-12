fe552dcdb44ab28fc41b62add358104e2167f10d
Deven-14 <deven.paramaj@gmail.com>
2026-07-12 21:42:49 +0100

    sol: 1507. Reformat Date

---
+++ 1507. Reformat Date/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        day = lambda x: x[:-2].zfill(2)
+        month = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
+        d, m, y = date.split()
+        return f"{y}-{month[m]}-{day(d)}"
