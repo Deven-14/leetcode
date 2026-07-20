5f1a6af2ed89a86fed2ebce0ea31362bd42aad07
Deven-14 <deven.paramaj@gmail.com>
2026-07-20 22:00:11 +0100

    sol: 1534. Count Good Triplets

--- 1534. Count Good Triplets/solution.py
+++ 1534. Count Good Triplets/solution.py
@@ -16,4 +16,29 @@ class Solution:
                         count += 1
         
         return count
-    
+
+
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        count = 0
+        n = len(arr)
+        prefix_sum_freq = [0] * 1001
+
+        for j in range(n):
+            for k in range(j + 1, n):
+                if abs(arr[j] - arr[k]) > b:
+                    continue
+                
+                lj, rj = arr[j] - a, arr[j] + a
+                lk, rk = arr[k] - c, arr[k] + c
+                l = max(0, lj, lk)
+                r = min(1000, rj, rk)
+
+                if l <= r:
+                    count += prefix_sum_freq[r] if l == 0 else prefix_sum_freq[r] - prefix_sum_freq[l - 1]
+            
+            for k in range(arr[j], 1001):
+                prefix_sum_freq[k] += 1
+            
+        return count
+
