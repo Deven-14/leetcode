8253b468b32ba969835c1b91890c44c27022c3a4
Deven-14 <deven.paramaj@gmail.com>
2026-07-30 22:21:32 +0100

    sol: 611. Valid Triangle Number

--- 611. Valid Triangle Number/solution.py
+++ 611. Valid Triangle Number/solution.py
@@ -37,3 +37,50 @@ class Solution:
         return count
 
 
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        freq = [0] * 1001
+        for num in nums:
+            freq[num] += 1
+        
+        j = 0
+        for i in range(0, 1001):
+            if freq[i] == 0: continue
+            nums[j:j+freq[i]] = [i] * freq[i]
+            j += freq[i]
+
+        n = len(nums)
+        count = 0
+
+        for i in range(n-1, 1, -1):
+            left, right = 0, i - 1
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    count += right - left
+                    right -= 1
+                else:
+                    left += 1
+        
+        return count
+        
+
+
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums = sorted(num for num in nums if num != 0)
+        n = len(nums)
+        count = 0
+
+        for i in range(n-1, 1, -1):
+            left, right = 0, i - 1
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    count += right - left
+                    right -= 1
+                else:
+                    left += 1
+        
+        return count
+
+
+
