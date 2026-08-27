f23316221b3e66dd149bcb221aa46a5e94c43b0b
Deven-14 <deven.paramaj@gmail.com>
2026-08-27 21:06:40 +0100

    sol: 1614. Maximum Nesting Depth of the Parentheses

---
+++ 1614. Maximum Nesting Depth of the Parentheses/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        depth = 0
+        max_depth = 0
+
+        for char in s:
+            if char == '(':
+                depth += 1
+            elif char == ')':
+                depth -= 1
+            if depth > max_depth:
+                max_depth = depth
+        
+        return max_depth
