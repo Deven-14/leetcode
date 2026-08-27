771b4802fcf50651348c71cdb117316fad548bbb
Deven-14 <deven.paramaj@gmail.com>
2026-08-27 20:52:07 +0100

    sol: 1608. Special Array With X Elements Greater Than or Equal X

--- 1608. Special Array With X Elements Greater Than or Equal X/solution.py
+++ 1608. Special Array With X Elements Greater Than or Equal X/solution.py
@@ -48,3 +48,22 @@ class Solution:
 # 0 element >= nums[0] => 0 element >= 
 
 
+import bisect
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        n = len(nums)
+        
+        for i in range(1, n + 1):
+            if nums[i - 1] >= i and (i == n or nums[i] < i):
+                return i
+        
+        return -1
+
+
+# i < nums[i] (in while loop so the negative condition of what we want)
+# => i >= nums[i] => (i + 1) >= nums[i] as i is idx
+# => x >= nums[x]
+# [4, 4, 3, 0, 0]
+# 0 eles >= nums[0] => 0 eles >= 4
+# 1 eles >= nums[1] => 
