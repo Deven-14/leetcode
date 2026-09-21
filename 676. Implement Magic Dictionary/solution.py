1cdecea862f625f13df72acebb99e5dd33e7d89f
Deven-14 <deven.paramaj@gmail.com>
2026-09-21 22:06:17 +0100

    sol: 676. Implement Magic Dictionary

--- 676. Implement Magic Dictionary/solution.py
+++ 676. Implement Magic Dictionary/solution.py
@@ -20,3 +20,77 @@ class MagicDictionary:
 # obj = MagicDictionary()
 # obj.buildDict(dictionary)
 # param_2 = obj.search(searchWord)
+
+
+
+
+# ! very very slow (below one)
+
+class MagicDictionary:
+
+    def __init__(self):
+        self.mdict = set()
+
+    def buildDict(self, dictionary: list[str]) -> None:
+        for word in dictionary:
+            for i in range(len(word)):
+                for char in string.ascii_lowercase:
+                    if char != word[i]:
+                        self.mdict.add(word[:i] + char + word[i + 1:])
+
+    def search(self, searchWord: str) -> bool:
+        return searchWord in self.mdict
+
+
+# Your MagicDictionary object will be instantiated and called as such:
+# obj = MagicDictionary()
+# obj.buildDict(dictionary)
+# param_2 = obj.search(searchWord)
+
+
+class MagicDictionary:
+
+    def __init__(self):
+        self.mdict = Counter()
+
+    def buildDict(self, dictionary: list[str]) -> None:
+        self.words = set(dictionary)
+        for word in dictionary:
+            for i in range(len(word)):
+                self.mdict[word[:i] + "*" + word[i + 1:]] += 1
+
+    def search(self, searchWord: str) -> bool:
+        for i in range(len(searchWord)):
+            word = searchWord[:i] + "*" + searchWord[i + 1:]
+            if self.mdict[word] > 1 or (self.mdict[word] == 1 and searchWord not in self.words):
+                return True
+        return False
+
+
+class MagicDictionary:
+
+    def __init__(self):
+        self.mdict = defaultdict(list)
+
+    def buildDict(self, dictionary: list[str]) -> None:
+        for word in dictionary:
+            self.mdict[len(word)].append(word)
+
+    def search(self, searchWord: str) -> bool:
+        for word in self.mdict[len(searchWord)]:
+            if self._match(word, searchWord):
+                return True
+        
+        return False
+    
+    def _match(self, word, searchWord):
+        if len(word) != len(searchWord): return False
+        mismatch = 0
+        
+        for a, b in zip(word, searchWord):
+            if a != b:
+                mismatch += 1
+                if mismatch > 1:
+                    return False
+        
+        return mismatch != 0
